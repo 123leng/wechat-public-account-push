c4f9ecaddab5f396843842ae14241682c6014b39
123leng <103876040+123leng@users.noreply.github.com>
2022-10-24 17:50:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'f06e2557b939099d830ab053e069cf61',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '浙江',
+  CITY: '宁波',
 
   USERS: [
     {
@@ -23,26 +23,26 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNiP56QjfzDGXOd7IZYG5yBC27JY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GXP-pPeisfn4XXkhZzr_qjh5JNA6MyBwRrO6kKGBFH4',
+      useTemplateId: '0YvKfkWcYIe5M8f_-CfIG78u7zfXIPHNWMVNyIThYuA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '胡慕颖', year: '2006', date: '07-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '冷嘉伟', year: '2003', date: '04-12',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '10-04',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-10-04' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'GXP-pPeisfn4XXkhZzr_qjh5JNA6MyBwRrO6kKGBFH4',
+  CALLBACK_TEMPLATE_ID: '0YvKfkWcYIe5M8f_-CfIG78u7zfXIPHNWMVNyIThYuA',
 
   CALLBACK_USERS: [
     {
